4f3fb6bdb291df1f1e5f923a725c2abc79f290e9
Ethan Holman <96ethanh@gmail.com>
2022-02-18 20:55:31 -0800

    Adding cognito provider details so TF doesnt try to update every time

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -57,6 +57,13 @@ resource "aws_cognito_identity_provider" "provider_google" {
     authorize_scopes = "profile email openid"
     client_id        = data.aws_ssm_parameter.google_client_id.value
     client_secret    = data.aws_ssm_parameter.google_client_secret.value
+    # this stuff comes from cognito -- adding it here so TF doesn't "detect" it everytime you run apply
+    attributes_url                = "https://people.googleapis.com/v1/people/me?personFields="
+    attributes_url_add_attributes = "true"
+    authorize_url                 = "https://accounts.google.com/o/oauth2/v2/auth"
+    oidc_issuer                   = "https://accounts.google.com"
+    token_request_method          = "POST"
+    token_url                     = "https://www.googleapis.com/oauth2/v4/token"
   }
   attribute_mapping = {
     email       = "email"
@@ -84,6 +91,12 @@ resource "aws_cognito_identity_provider" "provider_facebook" {
     authorize_scopes = "public_profile,email"
     client_id        = data.aws_ssm_parameter.facebook_client_id.value
     client_secret    = data.aws_ssm_parameter.facebook_client_secret.value
+    # this stuff comes from cognito -- adding it here so TF doesn't "detect" it everytime you run apply
+    attributes_url                = "https://graph.facebook.com/v12.0/me?fields="
+    attributes_url_add_attributes = "true"
+    authorize_url                 = "https://www.facebook.com/v12.0/dialog/oauth"
+    token_request_method          = "GET"
+    token_url                     = "https://graph.facebook.com/v12.0/oauth/access_token"
   }
   attribute_mapping = {
     username    = "id"
