4ff7d614ce7055295b51ecf9a42f6687d6a6c257
Ethan Holman <96ethanh@gmail.com>
2022-05-30 16:29:25 -0700

    Adding users dynamo table

--- terraform/database.tf
+++ terraform/database.tf
@@ -2,6 +2,9 @@ locals {
   videos_tags_pk     = "PK"
   videos_tags_sk     = "SK"
   videos_tags_userId = "userId"
+
+  users_pk = "PK"
+  users_sk = "SK"
 }
 
 resource "aws_dynamodb_table" "videos_and_tags" {
@@ -31,5 +34,23 @@ resource "aws_dynamodb_table" "videos_and_tags" {
     hash_key        = local.videos_tags_userId
     range_key       = local.videos_tags_sk
     projection_type = "ALL"
+  }
+}
+
+resource "aws_dynamodb_table" "users" {
+  name         = "Users"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = local.users_pk
+  range_key = local.users_sk
+
+  attribute {
+    name = local.users_pk
+    type = "S"
+  }
+
+  attribute {
+    name = local.users_sk
+    type = "S"
   }
 }
