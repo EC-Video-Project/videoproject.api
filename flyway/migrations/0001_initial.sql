0146a8d18ab2569bc93ddfcc2d604b9e8a81cc88
Ethan Holman <96ethanh@gmail.com>
2022-07-09 09:19:34 -0700

    Adding PK, FK, and indexes

--- flyway/migrations/0001_initial.sql
+++ flyway/migrations/0001_initial.sql
@@ -1,13 +1,13 @@
 create TABLE if NOT exists User (
     id int unsigned auto_increment primary key,
-    authId binary(16) not null, -- research, is an aws provided guid a string or binary type?
+    authId binary(16) not null,
     displayName varchar(50) not null,
     email varchar(50) not null,
     phone varchar(25) not null,
     employerMode bool not null,
     bio varchar(500) null,
     signupDate datetime not null,
-    profileLinks json not null
+    profileLinks json null
 );
 
 create table if NOT exists Video (
@@ -16,14 +16,14 @@ create table if NOT exists Video (
     userId int unsigned not null,
     createdDt datetime not null,
     updatedDt datetime not null,
-    isDeleted BOOL default 0,
+    isDeleted bool default 0,
     
     foreign key (userId) references User(id)
 );
 
 create table if NOT exists Posting (
     id int unsigned auto_increment primary key,
-    theStatus tinyint unsigned default 0,
+    itemStatus tinyint unsigned default 0,
     title varchar(50) not null,
     videoId int unsigned not null,
     userId int unsigned not null,
@@ -36,7 +36,7 @@ create table if NOT exists Posting (
 
 create table if NOT exists Introduction (
     id int unsigned auto_increment primary key,
-    theStatus tinyint unsigned default 0,
+    itemStatus tinyint unsigned default 0,
     videoId int unsigned not null,
     userId int unsigned not null,
     createdDt datetime not null,
@@ -49,7 +49,7 @@ create table if NOT exists Introduction (
 create table if NOT exists Application (
     id int unsigned auto_increment primary key,
     postingId int unsigned not null,
-    theStatus tinyint unsigned default 0,
+    itemStatus tinyint unsigned default 0,
     videoId int unsigned not null,
     userId int unsigned not null,
     createdDt datetime not null,
@@ -63,13 +63,14 @@ create table if NOT exists Application (
 create Table if NOT exists Tag (
     id mediumint unsigned auto_increment primary key,
     name varchar(25) not null,
-    type varchar(8) not null, -- industry,skill,location,hours
+    type varchar(8) not null -- industry,skill,location,hours
 );
 
 create table if NOT exists PostingTag (
     id int unsigned not null,
     tag mediumint unsigned not null,
 
+    primary key (id, tag),
     foreign key (id) references Posting(id),
     foreign key (tag) references Tag(id)
 );
@@ -78,14 +79,17 @@ create table if NOT exists IntroductionTag (
     id int unsigned not null,
     tag mediumint unsigned not null,
 
+    primary key (id, tag),
     foreign key (id) references Introduction(id),
     foreign key (tag) references Tag(id)
 );
 
 create table if NOT exists Star (
-    entityId int unsigned not null, -- how to index this
+    entityId int unsigned not null,
     userId int unsigned not null,
     
+    primary key (entityId, userId),
+    unique index (entityId),
     foreign key (userId) references User(id)
 );
 
@@ -93,16 +97,18 @@ create table if NOT exists View (
     entityId int unsigned not null,
     userId int unsigned not null,
 
+    primary key (entityId, userId),
+    unique index (entityId),
     foreign key (userId) references User(id)
 );
 
 create table if NOT exists Invitation (
     id int unsigned auto_increment primary key,
     sender int unsigned not null,
-    invitee int unsigned not null,
+    recipient int unsigned not null,
     accepted bool default 0,
-    message varchar(150) null,
+    message varchar(300) null,
     
     foreign key (sender) references User(id),
-    foreign key (invitee) references User(id)
+    foreign key (recipient) references User(id)
 );
