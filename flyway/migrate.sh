f303920a8900f86e5b5b11e7120d8bb3aafd2097
Ethan Holman <96ethanh@gmail.com>
2022-07-11 20:07:07 -0700

    Setup dockerized flyway. I miss working on linux

---
+++ flyway/migrate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash          
+
+FLYWAY_URL=$(aws ssm get-parameter --query Parameter.Value --output text --name db_url)
+FLYWAY_USER=admin
+FLYWAY_ADMINPW=$(aws ssm get-parameter --query Parameter.Value --with-decryption --output text --name db_adminPw)
+FLYWAY_DEFAULT_SCHEMA=codename
+
+# MSYS thing is to disable path rewrite for winblows git bash
+MSYS_NO_PATHCONV=1 docker run --rm \
+    -v `pwd`/conf:/flyway/conf \
+    -v `pwd`/migrations:/flyway/sql flyway/flyway:latest-alpine \
+    -url="jdbc:mysql://$FLYWAY_URL" \
+    -user="$FLYWAY_USER" \
+    -password="$FLYWAY_ADMINPW" \
+    -defaultSchema="$FLYWAY_DEFAULT_SCHEMA" \
+    migrate
